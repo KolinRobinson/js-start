542e261867ab76737f2adb39adb7ee6a61b75c3f
Taras <taras.senko.99@gmail.com>
2020-02-26 23:48:01 +0200

    modal

--- app/script.js
+++ app/script.js
@@ -1,34 +1,58 @@
 'use strict';
 
-let btn = document.querySelector('.btn'),
-    elem = document.querySelector('.box');
+// let btn = document.querySelector('.btn'),
+//     elem = document.querySelector('.box');
 
-function myAnimate(){
-    let pos = 0;
+// function myAnimate(){
+//     let pos = 0;
 
-    let id = setInterval(frame, 10);
+//     let id = setInterval(frame, 10);
 
-    function frame () {
-        if (pos == 400) {
-            clearInterval(id);
-        } else {
-            pos++;
-            elem.style.top = pos + 'px';
-            elem.style.left = pos + 'px';
-        }
-    }
+//     function frame () {
+//         if (pos == 400) {
+//             clearInterval(id);
+//         } else {
+//             pos++;
+//             elem.style.top = pos + 'px';
+//             elem.style.left = pos + 'px';
+//         }
+//     }
+// }
+
+// btn.addEventListener('click', myAnimate);
+
+// let btnBlock = document.querySelector('.btn-block'),
+//     btns = document.getElementsByTagName('button');
+
+// btnBlock.addEventListener('click', function(event) {
+//     if (event.target && event.target.matches('button.first') ){
+//         myAnimate();
+//     } else {
+
+//     }
+// });
+
+let btn = document.getElementById('btn'),
+    showModal = document.querySelector('#modal-show');
+
+function hello() {
+    btn.hidden = true;
 }
 
-btn.addEventListener('click', myAnimate);
 
-let btnBlock = document.querySelector('.btn-block'),
-    btns = document.getElementsByTagName('button');
 
-btnBlock.addEventListener('click', function(event) {
-    if (event.target && event.target.matches('button.first') ){
-        myAnimate();
-    } else {
+btn.hidden = false;
 
-    }
-});
+function show(){
+    let modal = document.querySelector('.modal_one');
+    modal.style.display = "block";
+}
 
+showModal.addEventListener('click', show);
+
+function hide(){
+    let modal = document.querySelector('.modal_one');
+    modal.style.display = "none";
+}
+
+btn.addEventListener('click', hide);
