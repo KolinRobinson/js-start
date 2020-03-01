9556cd40146aa2032e0782994100768286e84541
Taras <taras.senko.99@gmail.com>
2020-03-01 21:14:43 +0200

    my first tabs

--- app/script.js
+++ app/script.js
@@ -1,28 +1,27 @@
 'use strict';
 
-let push = document.querySelector('.push'),
-    radio = document.querySelectorAll('.radio-input'),
-    p = document.querySelectorAll('p'),
-    option = document.querySelectorAll('#my-select option');
+let tabBody = document.querySelectorAll('.tab-body'),
+    tab = document.querySelectorAll('.tab');
 
-console.log(option);
-
-push.addEventListener('click', function(){ 
-    console.log(radio);
-    //radio[2].hidden = true;
-    for (let i = 0; i < radio.length; i++){
-        if(radio[i].checked){
-            console.log(radio[i].value);
-        }
+function init(){
+    for(let i = 1; i < tabBody.length; i++){
+        tabBody[i].style.display = "none";
     }
-    for(let i = 0; i < option.length; i++){
-        if(option[i].selected){
-            console.log(option[i].value);
-        }
-    }
-});
-
-for(let i = 0; i < p.length; i++){
-    p[i].style.color = 'red';
 }
+init();
 
+tab.forEach(function(element){
+    element.onclick = showTabs;
+})
+
+function showTabs(){
+    let data = this.getAttribute('data');
+    for(let i = 0; i < tabBody.length; i++){
+        tabBody[i].style.display = "none";
+    }
+    document.querySelector(`.tab-body[data="${data}"]`).style.display = 'block';
+    for(let i = 0; i < tab.length; i++){
+        tab[i].classList.remove('active');
+    }
+    this.classList.add('active');
+}
