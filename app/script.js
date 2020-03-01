f0c4052ee85d83d04c6b24e3f5d52085c1f77e81
Taras <taras.senko.99@gmail.com>
2020-03-02 00:40:49 +0200

    keypress

--- app/script.js
+++ app/script.js
@@ -1,27 +1,44 @@
-'use strict';
+// let test = document.querySelector('#test');
 
-let tabBody = document.querySelectorAll('.tab-body'),
-    tab = document.querySelectorAll('.tab');
+// test.addEventListener('keypress', function(event) {
+//     if (event.keyCode < 48 || event.keyCode >57){
+//         console.log('недопустиміе символі');
+//     } 
+//     else {
+//         this.value += event.key;
+//     }
+//     return false;
+    // event.altKey = false;
+    // event.charCode = 97;
+    // event.code = 'KeyA';
+    // event.key = 'a';
+    // event.keyCode = '97';
 
-function init(){
-    for(let i = 1; i < tabBody.length; i++){
-        tabBody[i].style.display = "none";
-    }
-}
-init();
+// });
 
-tab.forEach(function(element){
-    element.onclick = showTabs;
-})
+// document.querySelector('#test').onkeypress = function(e){
+//     if (e.keyCode < 48 || e.keyCode >57){
+//         console.log('недопустиміе символі');
+//     } 
+//     else {
+//         this.value += e.key;
+//     }
+//     return false;
+// }
 
-function showTabs(){
-    let data = this.getAttribute('data');
-    for(let i = 0; i < tabBody.length; i++){
-        tabBody[i].style.display = "none";
-    }
-    document.querySelector(`.tab-body[data="${data}"]`).style.display = 'block';
-    for(let i = 0; i < tab.length; i++){
-        tab[i].classList.remove('active');
-    }
-    this.classList.add('active');
-}
+let left = 0,
+    bottom = 0;
+
+document.onkeypress = function () {
+    // console.log(e.keyCode);
+    // let sq = document.querySelector('.sq');
+    // if(e.keyCode == 100 || e.key == 'ArrowRight'){
+    //     left = left + 10;
+    //     sq.style.marginLeft = left + 'px';
+    // } else if (e.keyCode == 97) {
+    //     left = left - 10;
+    //     sq.style.marginLeft = left + 'px';
+    // }
+    
+
+};
